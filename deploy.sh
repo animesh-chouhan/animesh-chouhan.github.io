7e9761937772a7dc772ee2690a975d781f84d6d8
animesh-chouhan <animeshsingh.iitkgp@gmail.com>
2024-02-20 23:26:07 +0530

    rebuilding site Tuesday 20 February 2024 11:26:07 PM IST

--- deploy.sh
+++ deploy.sh
@@ -12,17 +12,17 @@ printf "\033[0;32mDeploying updates to GitHub website...\033[0m\n"
 
 cd ../
 
-# Add changes to git.
+# Add changes to git
 git add .
 
-# Commit changes.
+# Commit changes
 msg="rebuilding site $(date)"
 if [ -n "$*" ]; then
 	msg="$*"
 fi
 git commit -m "$msg"
 
-# Push source and build repos.
+# Push source and build repos
 git push origin master
 
 printf "\033[0;32mDone\033[0m\n"
